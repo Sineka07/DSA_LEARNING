a524df8f69527542fd406fca2ceb8b914f5bb105
Sineka07 <snehasenthilkumar91@gmail.com>
2026-06-17 18:39:33 +0530

    Day-3 DSA Learning

---
+++ Day_3/sort1s2s3s.java
@@ -0,0 +1,32 @@
+class Solution {
+    public void sort012(int[] arr) {
+        // code here
+        int n=arr.length;
+        int i=0;
+        int mid=0;
+        int j=n-1;
+        while(mid<=j)
+        {
+            if(arr[mid]==0)
+            {
+                int t=arr[mid];
+                arr[mid]=arr[i];
+                arr[i]=t;
+                i++;
+                mid++;
+            }
+            else if(arr[mid]==1)
+            {
+                mid++;
+            }
+            else
+            {
+                int t=arr[mid];
+                arr[mid]=arr[j];
+                arr[j]=t;
+                j--;
+            }
+        }
+        
+    }
+}
